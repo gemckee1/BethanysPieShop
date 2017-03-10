1a01600b26f87837b6f6a39df101f0f43819ff11
Greg McKee <greg.mckee@live.com>
2017-03-10 08:19:24 -0600

    Initial ommitt

--- BethanysPieShop/Startup.cs
+++ BethanysPieShop/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using BethanysPieShop.Models;
 
 namespace BethanysPieShop
 {
@@ -16,22 +17,18 @@ namespace BethanysPieShop
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<ICategoryRepository, MockCategoryRepository>();
+            services.AddTransient<IPieRepository, MockPieRepository>();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole();
-
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseExceptionHandler();
+            app.UseStaticFiles();
+            app.UseStatusCodePages();
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
